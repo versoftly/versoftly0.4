d0c448d6e49ce1fe4ada4c5dcff06b9374c407b5
Ramiro G Glez <mostlyphpsoftware@gmail.com>
2023-12-28 18:31:30 -0700

    icon en la parte donde se muestra el nombre del usuario logueado

--- core/vistas/privado.php
+++ core/vistas/privado.php
@@ -24,8 +24,21 @@
     <a id="top"></a>
         <header class="columna">
 
-            <div class="borde">
-                <h2>Bienvenid(a/o) Se&ntilde;(a/o)r
+            <div class="fila">
+                <h2 class="fila">Bienvenid(a/o) Se&ntilde;(a/o)r</2>
+                <h2 class="borde">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                        class="icon icon-tabler icon-tabler-user-circle" 
+                        width="44" height="44" viewBox="0 0 24 24" 
+                        stroke-width="1.5" stroke="#00bfd8" fill="none" 
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                        <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                        <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
+                    </svg>
+
                     <?php echo $_SESSION['user']; ?>
                 </h2>
             </div>
